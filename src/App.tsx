6e29a47327f2c2765c541ff25abd48db8b5f4407
manuelWeb <devries.webdesigner@gmail.com>
2018-11-18 18:45:53 +0100

    echec onClick -> typescript rale mais pourquoi ?

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import Keyboard from './Keyboard';
 import './App.css';
 
 class App extends Component {
+  // constructor (props:any) {
+  //   super(props)
+  //   this.hello = this.hello.bind(this)
+  // }
+
+  hello() {
+    console.log('hello');
+  }
   render() {
     return (
       <div className="App">
-        <Keyboard />
+        {/* <Keyboard onClick={this.hello} /> */}
+        <Keyboard maProp={this.hello} />
       </div>
     );
   }
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { isArray } from 'util';
 
 const alphabet: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-// const alphabet_: srting[] = [...alphabet]
-// const alphabet_: string[] = alphabet.split('')
-const alphabet_: any = [...alphabet]
-// alphabet_.push(alphabet)
+// typescript renommer alphabet rapport au type
+const alphabet_: string[] = [...alphabet]
 
 console.log(isArray( alphabet_ ), alphabet_);
 
-const Keyboard = () => (
-  <span>{alphabet}</span>
-)
+const Keyboard = () => {
+  const clavier: JSX.Element = (
+    <div className="keyboard_container">
+      {
+        alphabet_.map( (letter,idx) =>
+          (
+            <span key={idx}>{letter}</span>
+          )
+        )
+      }
+    </div>
+  )
+  return clavier
+}
+
 
 export default Keyboard
