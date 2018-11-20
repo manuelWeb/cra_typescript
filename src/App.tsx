66274318d1565481f324f0423f6e81b71b2ef3f3
manuelWeb <devries.webdesigner@gmail.com>
2018-11-20 20:35:41 +0100

    deport de handleKeyClick en dehors de la class app; TODO definir strictement this

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import Keyboard from './Keyboard';
 
 import './App.css';
 
-// function handleKeyClick ( this:void, letter:string, idx: number ): void {
-//   const letters:string[] = this.state.letters
-//   const idxIsClicked:number[] = this.state.idxIsClicked
-//   // add clicked letter to letters and setstate
-//   letters.push(letter)
-//   console.log(`typeof letters:${typeof letters }`, `typeof letter:${typeof letter} letters:${letters} idx: ${idx}`);
-//   this.setState({letters: letters})
-// }
-interface ITestProps {
-  onClick: ( arg: any ) => void,
+function handleKeyClick ( this: any, letter:string, idx: number ): void {
+  const letters:string[] = this.state.letters
+  const idxIsClicked:number[] = this.state.idxIsClicked
+  // add clicked letter to letters and setstate
+  letters.push(letter)
+  console.log(`typeof letters:${typeof letters }`, `typeof letter:${typeof letter} letters:${letters} idx: ${idx}`);
+  this.setState({letters: letters})
+  this.setState({idxIsClicked: letters})
 }
 
+// interface ITestProps {
+//   onClick: ( arg: any ) => void,
+// }
 function test(this: any, secArg?: any) {
-  console.log(secArg);
-  console.log(this);
+  console.log(`secArg: ${secArg}`);
+  console.log(`this:${this}`);
 }
 
 class App extends Component {
@@ -27,22 +28,25 @@ class App extends Component {
   }
   constructor (props:{}) {
     super(props)
-    this.handleKeyClick = this.handleKeyClick.bind(this)
+    // this.handleKeyClick = this.handleKeyClick.bind(this)
+    // this.handleKeyClick = handleKeyClick.bind(this)
   }
-  handleKeyClick ( letter:string, idx: number ) {
-    const letters:string[] = this.state.letters
-    const idxIsClicked:number[] = this.state.idxIsClicked
-    // add clicked letter to letters and setstate
-    letters.push(letter)
-    idxIsClicked.push(idx)
-    console.log(`typeof letters:${typeof letters }`, `typeof letter:${typeof letter} letters:${letters} idx: ${idx}`);
-    this.setState({letters: letters, idxIsClicked: idxIsClicked})
-  }
+  // handleKeyClick ( letter:string, idx: number ) {
+  //   const letters:string[] = this.state.letters
+  //   const idxIsClicked:number[] = this.state.idxIsClicked
+  //   // add clicked letter to letters and setstate
+  //   letters.push(letter)
+  //   idxIsClicked.push(idx)
+  //   console.log(`typeof letters:${typeof letters }`, `typeof letter:${typeof letter} letters:${letters} idx: ${idx}`);
+  //   this.setState({letters: letters, idxIsClicked: idxIsClicked})
+  // }
   render() {
     return (
       <div className="App">
-        <Keyboard onClick={this.handleKeyClick} />
+        {/* <Keyboard onClick={this.handleKeyClick} /> */}
+        <Keyboard onClick={handleKeyClick.bind(this)} />
         <div onClick={ () => test([...this.state.letters]) }>Test</div>
+        {/* <div onClick={ () => test(this) }>Test</div> */}
         {/* <Keyboard /> */}
       </div>
     );
